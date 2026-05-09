50faf30aed86e72a647c9ac7ef6d19c3dde481f3
Enzo Pinheiro Peralva <pessoalenzopinheiro@gmail.com>
2026-05-09 11:18:46 -0300

    Homework decorators

---
+++ aula30.py
@@ -0,0 +1,17 @@
+def check(func):
+    def wrapper(*args, **kwargs):
+        resultado = func(*args, **kwargs)
+        print(f'Nome da função chamada: {func.__name__}')
+        print(f'Argumentos posicionais recebidos: {args}')
+        print(f'Argumentos nomeados recebidos: {kwargs}')
+        print(f'Resultado da função: ', resultado)
+        return resultado
+    return wrapper
+
+
+@check
+def my_sum(x: float, y: float) -> float:
+    return x + y
+
+test = my_sum(1, 2)
+print(test)
