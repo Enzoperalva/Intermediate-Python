7d632d019f5fb3ab9bac8fedf87d2b6739dfa4a6
Enzo Pinheiro Peralva <pessoalenzopinheiro@gmail.com>
2026-05-11 08:45:56 -0300

    Atividades otavio miranda

--- aula29.py
+++ aula29.py
@@ -6,11 +6,11 @@ def monitoring_time(func):
         start_time = time()
         print('Iniciando execução...')
         sleep(2)
-        executng_func = func(*args, **kwargs)
+        save_func = func(*args, **kwargs)
         end_time = time()
         duration = end_time - start_time
         print(f'Execução finalizada. Tempo de execução: {duration:.2f}')
-        return executng_func
+        return save_func
 
     return wrapper
 
@@ -22,13 +22,3 @@ def my_sum(x: float, y: float) -> float:
 
 test = my_sum(19, 4)
 print(test)
-
-
-# @monitoring_time
-# def pegar_cotacao_dolar():
-#     link = f"https://economia.awesomeapi.com.br/last/USD-BRL"
-#     requisicao = requests.get(link)
-#     requisicao = requisicao.json()
-#     return f'{requisicao['USDBRL']['bid']}'
-
-# print(pegar_cotacao_dolar())
--- aula30.py
+++ aula30.py
@@ -1,9 +1,9 @@
 def check(func):
     def wrapper(*args, **kwargs):
-        resultado = func(*args, **kwargs)
         print(f'Nome da função chamada: {func.__name__}')
         print(f'Argumentos posicionais recebidos: {args}')
         print(f'Argumentos nomeados recebidos: {kwargs}')
+        resultado = func(*args, **kwargs)
         print(f'Resultado da função: ', resultado)
         return resultado
     return wrapper
