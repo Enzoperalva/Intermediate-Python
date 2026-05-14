39ba8ff25a96a18fc685d8c24f1097d364fd7f89
Enzo Pinheiro Peralva <pessoalenzopinheiro@gmail.com>
2026-05-14 08:55:55 -0300

    Exercicio groupby

--- aula35.py
+++ aula35.py
@@ -1,2 +1,22 @@
-arquivo = open('contatos.txt', 'w')
-arquivo.write('ENZINHO')
+from itertools import groupby
+
+palavras = [
+    'amor',
+    'avião',
+    'banana',
+    'bola',
+    'casa',
+    'carro',
+    'dado',
+    'dinheiro',
+    'elefante',
+]
+
+organization = sorted(palavras, key=lambda p: p[0])
+grup = groupby(organization, key= lambda p: p[0])
+
+for chave, valor in grup:
+    print(chave + ':')
+    for item in valor: 
+        print(item)
+    print()
