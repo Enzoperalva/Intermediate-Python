ba698c4fe1189aed4cdd86036224b46a30f76c32
Enzo Pinheiro Peralva <pessoalenzopinheiro@gmail.com>
2026-06-09 08:46:35 -0300

    Questoes heranca multipla|problema diamante

---
+++ aula73.py
@@ -0,0 +1,23 @@
+# PROBLEMA DIAMANTE
+class A:
+    ...
+    def quem_sou(self):
+        print("Sou A")
+
+class B(A):
+    ...
+    # def quem_sou(self):
+    #     print("Sou B")
+
+class C(A):
+    ...
+    # def quem_sou(self):
+    #     print("Sou C")
+
+class D(B, C):
+    ...
+    # def quem_sou(self):
+    #     print("Sou D")
+
+d = D()
+d.quem_sou()
